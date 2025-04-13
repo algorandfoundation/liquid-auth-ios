4ba9eeec8da426f5abceaf91896efd18c1b1befc
HashMapsData2Value <83883690+HashMapsData2Value@users.noreply.github.com>
2025-04-13 21:05:03 +0200

    feat: app scans qr code

---
+++ liquid-auth-ios/liquid-auth-ios/liquid_auth_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  liquid_auth_iosApp.swift
+//  liquid-auth-ios
+//
+//  Created by Yared Efrem Afework on 2025-04-11.
+//
+
+import SwiftUI
+
+@main
+struct liquid_auth_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ liquid-auth-ios/liquid-auth-iosTests/liquid_auth_iosTests.swift
@@ -0,0 +1,17 @@
+//
+//  liquid_auth_iosTests.swift
+//  liquid-auth-iosTests
+//
+//  Created by Yared Efrem Afework on 2025-04-11.
+//
+
+import Testing
+@testable import liquid_auth_ios
+
+struct liquid_auth_iosTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
